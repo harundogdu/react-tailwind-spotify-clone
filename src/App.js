ffcf528e04e3b2bcd9b8521ff7c365e8a999136d
Harun Doğdu <harundogdu06@gmail.com>
2022-01-04 01:57:52 +0300

    Added tailwind css.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
