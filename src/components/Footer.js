db976ef64eb9c5b65e52266a2d1f5deb8ee1cf1c
Harun Doğdu <harundogdu06@gmail.com>
2022-01-04 20:32:53 +0300

    created Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='w-full h-28 flex flex-shrink-0 bg-bgFooter border-t border-bgLink'>
+            Footer
+        </div>
+    )
+}
+
+export default Footer
