beb6e6bf98212d50cd79bff71263cd89c6ae4601
Harun Doğdu <harundogdu06@gmail.com>
2022-01-04 20:31:51 +0300

    new css settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgBase: "#1a1a1a",
+        bgSideBar: "#121212",
+        bgLink: "#282828",
+        bgText: "#b3b3b3",
+        bgFooter:"#181818"
+      },
+      minHeight: {
+        "fullSizeForSideBar": "86vh",
+      },
+    },
   },
   plugins: [],
 };
