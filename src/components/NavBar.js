fb3da467ca335e50dce4310286a5a9f83036faf5
Harun Doğdu <harundogdu06@gmail.com>
2022-01-05 19:27:06 +0300

    The navbar is given "position:sticky".

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,9 +6,20 @@ import { useMatch } from "react-router-dom";
 const Navbar = () => {
   const searchRoute = useMatch("/search");
   const collectionRoute = useMatch("/collection");
+  const [scrollY, setScrollY] = React.useState(0);
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScrollY(window.scrollY);
+    });
+  }, [scrollY]);
 
   return (
-    <div className="flex items-center justify-between h-16  py-4 px-8">
+    <div
+      className={`flex items-center justify-between h-16 py-4 px-8 w-full z-30 sticky top-0 transition-colors border-white border-b border-opacity-0 ${
+        scrollY > 0 ? " bg-bgNavbarActive border-opacity-10" : ""
+      } `}
+    >
       <div className="flex items-center justify-center gap-x-4">
         <LeftNavBar />
         {searchRoute && <div>Arama sayfası</div>}
