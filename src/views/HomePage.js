547f9e28b29252740c16e0b45760eedb2ef1e419
Harun Doğdu <harundogdu06@gmail.com>
2022-01-05 19:28:48 +0300

    homepage designed

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import UpperSection from "components/Content/UpperSection";
+import LowerSection from "components/Content/LowerSection";
 const HomePage = () => {
   return (
-    <div className="flex flex-col bg-transparent ">
-      <div className="grid gap-6 pt-6 px-8">
+    <div className="flex flex-col ">
+      <div className="grid gap-6 pt-6 px-8 ">
         <UpperSection title={"İyi akşamlar"} />
+        <LowerSection />
       </div>
     </div>
   );
