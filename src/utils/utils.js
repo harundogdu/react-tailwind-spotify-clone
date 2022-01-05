9d22b34a0fd59063682d38b8ae5db234eb30feb4
Harun Doğdu <harundogdu06@gmail.com>
2022-01-05 19:27:57 +0300

    songs array changed and added utils.js

---
+++ src/utils/utils.js
@@ -0,0 +1,27 @@
+/* Rastgele Müzikler Getiren Liste */
+
+function randomArrayShuffle(array, seed = false, compareArray = false) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  if (compareArray) {
+    array = Math.random() * 10 >= 5 ? array : compareArray;
+  }
+
+  return seed
+    ? Object.values(array).slice(
+        0,
+        Math.random() * 5 + 2 >= 6 ? 6 : Math.random() * 5 + 3
+      )
+    : array;
+}
+
+export { randomArrayShuffle };
