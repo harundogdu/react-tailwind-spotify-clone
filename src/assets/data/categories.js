bf3c1b68321331edf62624f4303bc2ae23cfb742
Harun Doğdu <harundogdu06@gmail.com>
2022-01-07 21:12:26 +0300

    categories data created

---
+++ src/assets/data/categories.js
@@ -0,0 +1,181 @@
+const favouriteCategories = [
+  {
+    id: 1,
+    title: "Pop",
+    cover: "https://t.scdn.co/images/0a74d96e091a495bb09c0d83210910c3",
+    color: "#79559f",
+  },
+  {
+    id: 2,
+    title: "Hip hop",
+    cover: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+    color: "#ba5d08",
+  },
+  {
+    id: 3,
+    title: "Rock",
+    cover:
+      "https://charts-images.scdn.co/assets/locale_en/regional/weekly/region_global_default.jpg",
+    color: "#e51d32",
+  },
+  {
+    id: 4,
+    title: "Dans ve Elektronik",
+    cover: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+    color: "#dc148b",
+  },
+  {
+    id: 1,
+    title: "Pop",
+    cover: "https://t.scdn.co/images/0a74d96e091a495bb09c0d83210910c3",
+    color: "#79559f",
+  },
+  {
+    id: 2,
+    title: "Hip hop",
+    cover: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+    color: "#ba5d08",
+  },
+  {
+    id: 3,
+    title: "Rock",
+    cover:
+      "https://charts-images.scdn.co/assets/locale_en/regional/weekly/region_global_default.jpg",
+    color: "#e51d32",
+  },
+  {
+    id: 4,
+    title: "Dans ve Elektronik",
+    cover: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+    color: "#dc148b",
+  },
+];
+
+const categories = [
+  {
+    id: 1,
+    title: "Podcast'ler",
+    cover: "https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5",
+    color: "#27856a",
+  },
+  {
+    id: 2,
+    title: "Senin İçin Hazırlandı",
+    cover: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+    color: "#1e3164",
+  },
+  {
+    id: 3,
+    title: "Listeler",
+    cover:
+      "https://charts-images.scdn.co/assets/locale_en/regional/weekly/region_global_default.jpg",
+    color: "#8c67ab",
+  },
+  {
+    id: 4,
+    title: "Yeni Çıkanlar",
+    cover: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+    color: "#e8105b",
+  },
+  {
+    id: 5,
+    title: "Keşfet",
+    cover: "https://t.scdn.co/images/d0fb2ab104dc4846bdc56d72b0b0d785.jpeg",
+    color: "#ba5d08",
+  },
+  {
+    id: 6,
+    title: "Podcast'ler",
+    cover: "https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5",
+    color: "#27856a",
+  },
+  {
+    id: 7,
+    title: "Senin İçin Hazırlandı",
+    cover: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+    color: "#1e3164",
+  },
+  {
+    id: 8,
+    title: "Listeler",
+    cover:
+      "https://charts-images.scdn.co/assets/locale_en/regional/weekly/region_global_default.jpg",
+    color: "#8c67ab",
+  },
+  {
+    id: 9,
+    title: "Yeni Çıkanlar",
+    cover: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+    color: "#e8105b",
+  },
+  {
+    id: 10,
+    title: "Keşfet",
+    cover: "https://t.scdn.co/images/d0fb2ab104dc4846bdc56d72b0b0d785.jpeg",
+    color: "#ba5d08",
+  },
+  {
+    id: 11,
+    title: "Podcast'ler",
+    cover: "https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5",
+    color: "#27856a",
+  },
+  {
+    id: 12,
+    title: "Senin İçin Hazırlandı",
+    cover: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+    color: "#1e3164",
+  },
+  {
+    id: 13,
+    title: "Listeler",
+    cover:
+      "https://charts-images.scdn.co/assets/locale_en/regional/weekly/region_global_default.jpg",
+    color: "#8c67ab",
+  },
+  {
+    id: 14,
+    title: "Yeni Çıkanlar",
+    cover: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+    color: "#e8105b",
+  },
+  {
+    id: 15,
+    title: "Keşfet",
+    cover: "https://t.scdn.co/images/d0fb2ab104dc4846bdc56d72b0b0d785.jpeg",
+    color: "#ba5d08",
+  },
+  {
+    id: 16,
+    title: "Podcast'ler",
+    cover: "https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5",
+    color: "#27856a",
+  },
+  {
+    id: 17,
+    title: "Senin İçin Hazırlandı",
+    cover: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+    color: "#1e3164",
+  },
+  {
+    id: 18,
+    title: "Listeler",
+    cover:
+      "https://charts-images.scdn.co/assets/locale_en/regional/weekly/region_global_default.jpg",
+    color: "#8c67ab",
+  },
+  {
+    id: 19,
+    title: "Yeni Çıkanlar",
+    cover: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+    color: "#e8105b",
+  },
+  {
+    id: 20,
+    title: "Keşfet",
+    cover: "https://t.scdn.co/images/d0fb2ab104dc4846bdc56d72b0b0d785.jpeg",
+    color: "#ba5d08",
+  },
+];
+
+export { categories, favouriteCategories };
