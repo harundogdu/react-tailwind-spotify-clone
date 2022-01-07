5246d786d0a42ee9f2e3797544503910217d291a
Harun Doğdu <harundogdu06@gmail.com>
2022-01-07 21:13:37 +0300

    SearchPage designed

--- src/views/SearchPage.js
+++ src/views/SearchPage.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import { Category, FavouritesCategories } from "components/Search/Category";
+import React from "react";
+import {
+  categories as allCategories,
+  favouriteCategories as allFavoriteCategories,
+} from "assets/data/categories";
 
 const SearchPage = () => {
-    return (
-        <div>
-            SearchPage
-        </div>
-    )
-}
+  const [categories, setCategories] = React.useState([]);
+  const [favouritesCategories, setFavouritesCategories] = React.useState([]);
 
-export default SearchPage
+  React.useEffect(() => {
+    setCategories(allCategories);
+    setFavouritesCategories(allFavoriteCategories);
+  }, []);
+
+  return (
+    <div className="flex flex-col ">
+      <div className="grid gap-6 pt-2 px-8 ">
+        <FavouritesCategories categories={favouritesCategories} />
+        <Category categories={categories} />
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage;
