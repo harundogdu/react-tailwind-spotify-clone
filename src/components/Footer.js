690421f1fbcadbbc9df36b5facfd879c0e11527b
Harun Doğdu <43449361+harundogdu@users.noreply.github.com>
2022-01-08 18:52:55 +0300

    fixed prettier logs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import React, { useEffect } from "react";
 import { useAudio } from "react-use";
 import { useDispatch, useSelector } from "react-redux";
